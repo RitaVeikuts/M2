aa857bbe3c6180128a9b223ebf891b4c0c5dc283
RitaVeikuts <ritaveikuts@gmail.com>
2019-04-10 18:46:11 +0300

    jl

---
+++ src/homew1/Main.java
@@ -0,0 +1,15 @@
+package homew1;
+
+public class Main {
+    public static void main(String[] args) {
+        int x=2; {
+            if ((x*2)>20) {
+                System.out.println(x);
+            } else  if ((x*2)<20) {
+                System.out.println(-x);
+            } else if ((x*2)==20) {
+                System.out.println('0');
+            }
+        }
+    }
+}
