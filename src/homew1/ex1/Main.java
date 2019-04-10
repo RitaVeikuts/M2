231e25a01d2ef1fc4ed5f1c1a1ecd9a9a05bcd8c
RitaVeikuts <ritaveikuts@gmail.com>
2019-04-10 19:21:06 +0300

    l

--- src/homew1/ex1/Main.java
+++ src/homew1/ex1/Main.java
@@ -1,9 +1,10 @@
-package homew1.lesson3;
+package homew1.ex1;
 
 public class Main {
     public static void main(String[] args) {
-        int x=2; {
-            if ((x*2)>20) {
+        int x = 2;
+        {
+         /*   if ((x*2)>20) {
                 System.out.println(x);
             } else  if ((x*2)<20) {
                 System.out.println(-x);
@@ -11,6 +12,18 @@ public class Main {
                 System.out.println('0');
             }
          //123
+        }*/
+
+int result=0;
+
+if (x*2<20)
+    result = -x;
+else if (x*2>20)
+    result = x;
+else
+    result = 0;
+
+            System.out.println(result);
         }
     }
 }
--- src/homew1/ex2/Main.java
+++ src/homew1/ex2/Main.java
@@ -2,11 +2,16 @@ package homew1.ex2;
 
 public class Main {
     public static void main(String[] args) {
-        int x=1; {
-            do {
+        int x=1;
+        {
+          /*  do {
                 System.out.println("Это "+x+"-я строка");
                 x++;
             } while (x!=16);
+        }*/
+            for (int i = 1; i <= 15; i++)
+                System.out.println("Это " + i + "-я строка");
+
         }
     }
 }
