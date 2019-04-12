110c1abdcfd0191c214060693405a730d0ade890
RitaVeikuts <ritaveikuts@gmail.com>
2019-04-12 19:53:55 +0300

    bb

---
+++ src/lesson4/Main.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+
+        System.out.println(person.b);
+        person.print();
+
+    }
+}
---
+++ src/lesson4/Person.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+
+public class Person {
+
+    private int a =3;
+    public int b =4;
+
+    public void print() {
+        System.out.println(this.a + " " +this.b);
+
+    }
+}
